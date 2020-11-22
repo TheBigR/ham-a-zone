ff7e532bf9326c6bdc16eff28eea1ebc34c69786
TheBigR <nevoroy@gmail.com>
2020-11-22 12:49:38 +0200

    added selected  product reducer

---
+++ src/reducers/selectedProductReducer.js
@@ -0,0 +1,19 @@
+import { SHOW_PRODUCT_DETAILS, UPDATE_QUANTITY } from '../actions/types';
+
+const INITIAL_STATE= {
+    selectedProduct: null,
+    selectedProductQuantity: 1
+}
+
+const selectedProductReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case SHOW_PRODUCT_DETAILS:
+            return {...state, selectedProduct: action.payload}
+        case UPDATE_QUANTITY:
+            return  {...state, selectedProductQuantity: action.payload}
+        default:
+            return state;
+    }
+}
+
+export default selectedProductReducer;
