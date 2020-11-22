e2eeca12899a7125323688ce3ad665e2812460b9
TheBigR <nevoroy@gmail.com>
2020-11-22 12:38:49 +0200

    adding types

---
+++ src/actions/types.js
@@ -0,0 +1,53 @@
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+export const SHOW_PRODUCT_DETAILS = 'SHOW_PRODUCT_DETAILS';
+export const ADD_QUANTITY = 'ADD_QUANTITY';
+export const SUB_QUANTITY = 'SUB_QUANTITY';
+export const SELECT_PRODUCT_IN_CART = 'SELECT_PRODUCT_IN_CART';
+export const ADD_TO_CART = 'ADD_PRODUCT_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const REMOVE_FROM_TOTAL = 'REMOVE_FROM_TOTAL';
+export const UPDATE_QUANTITY  = 'UPDATE_QUANTITY';
+export const FETCH_IMAGES = 'FETCH_IMAGES';
+
+export const QTY = [
+    {
+        label: '1',
+        value: 1
+    },
+    {
+        label: '2',
+        value: 2
+    },
+    {
+        label: '3',
+        value: 3
+    },
+    {
+        label: '4',
+        value: 4
+    },
+    {
+        label: '5',
+        value: 5
+    },
+    {
+        label: '6',
+        value: 6
+    },
+    {
+        label: '7',
+        value: 7
+    },
+    {
+        label: '8',
+        value: 8
+    },
+    {
+        label: '9',
+        value: 9
+    },
+    {
+        label: '10',
+        value: 10
+    }
+]
