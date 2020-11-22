5713515cc45c5c5584e052f371ce417065089e6f
TheBigR <nevoroy@gmail.com>
2020-11-22 12:46:31 +0200

    added  cart reducer

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,48 @@
+import _ from 'lodash';
+import { ADD_QUANTITY, ADD_TO_CART, REMOVE_FROM_CART, REMOVE_FROM_TOTAL, SUB_QUANTITY } from '../actions/types';
+
+const initialState = [];   
+const quantityInitialState = {};
+const totalItems = 0;
+
+export const cartReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case ADD_TO_CART:            
+            return [...state, action.payload.product.id];     
+        case REMOVE_FROM_CART:            
+            return state.filter(element =>  element !== action.payload.id);
+        default:
+            return state;
+    }
+};
+
+
+export const quantityReducer = (state = quantityInitialState, action) => {
+    switch(action.type) {
+        case ADD_TO_CART:                       
+            return {...state, [action.payload.product.id]: 1};
+        case REMOVE_FROM_CART:
+            return _.omit(state, action.payload.id)
+        case ADD_QUANTITY:            
+            return {...state, [action.payload.id]: ++state[action.payload.id]};
+        case SUB_QUANTITY:
+            return {...state, [action.payload.id]: --state[action.payload.id]};
+        default:
+            return state;
+    }
+}
+
+export const totalItemsReducer = (state = totalItems,  action) => {
+    switch(action.type) {
+        case ADD_TO_CART:
+            return ++state;
+        case  REMOVE_FROM_TOTAL:
+            return state - action.payload;
+        case ADD_QUANTITY:
+            return ++state;
+        case SUB_QUANTITY:
+            return --state;
+        default:
+            return state;
+    }
+}
