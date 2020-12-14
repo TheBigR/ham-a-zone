08436a8f642fdf7f8356ba60716fd92b3d71355d
TheBigR <nevoroy@gmail.com>
2020-12-14 22:18:38 +0200

    more cosmetics

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route } from  'react-router-dom';
-import Header from './Header';
-import ProductList from './ProductList';
-import ProductDetails from './ProductDetails';
-import Cart from  './Cart';
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './Header'
+import ProductList from './ProductList'
+import ProductDetails from './ProductDetails'
+import Cart from './Cart'
 
-
-const App  = () => {  
-
-return (
+const App = () => {
+  return (
     <div className="ui container">
-        <BrowserRouter>
-            <div>
-                <Header  />
-                <Route path="/" exact component={ProductList} />
-                <Route path="/product/:id" component={ProductDetails} />
-                <Route path="/cart" exact component={Cart} />
-            </div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={ProductList} />
+          <Route path="/product/:id" component={ProductDetails} />
+          <Route path="/cart" exact component={Cart} />
+        </div>
+      </BrowserRouter>
     </div>
-    ); 
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-
-
-const Header  = ({totalItems}) => {  
-
-return (
-    <div  className="ui secondary pointing menu"> 
-        <Link to="/" className="item">
-            Home           
-        </Link>
-        <div className="right menu">
+const Header = ({ totalItems }) => {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Home
+      </Link>
+      <div className="right menu">
         <Link to="/cart" className="item">
-            <i className="shop icon"></i>
-            {totalItems}
+          <i className="shop icon"></i>
+          {totalItems}
         </Link>
-        
-        </div>
+      </div>
     </div>
-    ); 
-};
-
-const mapStateToProps = (state) => {
-    return {        
-        totalItems: state.totalItems
-    }
+  )
 }
 
-export default connect (mapStateToProps)(Header);
+const mapStateToProps = (state) => {
+  return {
+    totalItems: state.totalItems,
+  }
+}
+
+export default connect(mapStateToProps)(Header)
